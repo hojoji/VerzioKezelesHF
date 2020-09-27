7a8f08af2a9c4af05b2d4c0a5e25fec54c36ec32
hojoji <horoland1112@gmail.com>
2020-09-27 10:58:40 +0200

    2.feladat: beolvasott 10 szám átlaga

--- Program.cs
+++ Program.cs
@@ -10,6 +10,15 @@ namespace VerzioKezelesHF
     {
         static void Main(string[] args)
         {
+           int ossz = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Kérem a {0}. számot",i+1);
+                ossz += Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("A számok átlaga: "+ossz/10);
+
+            Console.ReadLine();
         }
     }
 }
